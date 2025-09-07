4b2bc30b1f665e127da7742a7390980bb3f2c8a5
Arthgar25 <desarthgar@gmail.com>
2025-09-07 16:06:54 -0700

    Se modifico la clase GanaleJuego para que solo contenga informacion relativa al juego en general. Se creo la clase Jugador que solo contiene atributos y metodos relativos a las acciones de los jugadores

--- LCRGame-master/GanaleJuego.java
+++ LCRGame-master/GanaleJuego.java
@@ -7,54 +7,11 @@
  */
 public class GanaleJuego
 {
-    DadoLCR dado1, dado2;
-    int fichas = 0;
     int turno = 0;
     boolean juegoTerminado = false;
 
     public GanaleJuego(){
-        dado1 = new DadoLCR();
-        dado2 = new DadoLCR();
+
     }
-
-    public void lanzarDados(){
-
-        if(juegoTerminado)return;
-
-        dado1.lanzar();
-        dado2.lanzar();
-
-        switch(dado1.getValor()){
-            case 'L':
-                fichas--;
-                break;
-            case 'R':
-                fichas--;
-                fichas--;
-                break;
-            case '*':
-                fichas++;
-                break;
-        }
-
-        switch(dado2.getValor()){
-            case 'L':
-                fichas--;
-                break;
-            case 'R':
-                fichas--;
-                fichas--;
-                break;
-            case '*':
-                fichas++;
-                break;
-        }
-
-        if(dado1.getValor() == 'C' || dado2.getValor() == 'C'){
-            fichas *= 2;
-        }
-
-        turno++;
-        if(fichas >= 6 || turno >= 10)juegoTerminado = true;
-    }
+    
 }
---
+++ LCRGame-master/Jugador.java
@@ -0,0 +1,61 @@
+
+/**
+ * Write a description of class Jugador here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Jugador
+{
+    DadoLCR dado1;
+    DadoLCR dado2;
+    int fichas = 0;
+    int id;
+    public Jugador(int id){
+        dado1 = new DadoLCR();
+        dado2 = new DadoLCR();
+        this.id = id;
+    }
+
+    public void lanzarDados(){
+        dado1.lanzar(); 
+        dado2.lanzar();
+
+        procesarResultados(dado1.getValor(), dado2.getValor());
+
+    }
+
+    private void procesarResultados(char resultado1, char resultado2){
+        switch(resultado1){
+            case 'L':
+                fichas--;
+                break;
+            case 'R':
+                fichas--;
+                fichas--;
+                break;
+            case '*':
+                fichas++;
+                break;
+        }
+        switch(resultado2){
+            case 'L':
+                fichas--;
+                break;
+            case 'R':
+                fichas--;
+                fichas--;
+                break;
+            case '*':
+                fichas++;
+                break;
+        }
+        if(fichas<0) fichas = 0;
+        
+        if(dado1.getValor() == 'C' || dado2.getValor() == 'C'){
+            fichas *= 2;
+        }
+    }
+    
+
+}
